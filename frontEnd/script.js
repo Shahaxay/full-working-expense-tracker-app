ebd22d7bfee143d4ea735b5a30c5056e91b0028a
Akshayshah47 <removeit61@gmail.com>
2023-06-24 15:25:40 +0530

    email exist message added to screen in signup page

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -2,6 +2,7 @@ var uname=document.getElementById('name');
 var email=document.getElementById('email');
 var password=document.getElementById('password');
 var frm=document.getElementById('frm');
+var dest=document.getElementById('dest');
 console.log(document);
 
 frm.addEventListener('submit',async (e)=>{
@@ -13,8 +14,13 @@ frm.addEventListener('submit',async (e)=>{
     };
     try{
         const result=await axios.post("http://localhost:3000/user/signup",signupObj)
-        console.log(result);
+        console.log(result.status);
     }catch(err){
+        dest.innerText='email already exists';
+        dest.style.color='red';
+        setTimeout(() => {
+            dest.innerText='';   
+        }, 2000);
         console.log(err);
     }
 })
