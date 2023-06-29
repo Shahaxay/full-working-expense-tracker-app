5523cef875ba8ad0ee20ac4d2a9ce3c7fd289a52
Akshayshah47 <removeit61@gmail.com>
2023-06-29 13:24:41 +0530

    own sslcertificate generation and ssl encryption

--- backEnd/app.js
+++ backEnd/app.js
@@ -5,6 +5,7 @@ const helmet=require('helmet');
 const morgan=require('morgan')
 const fs=require('fs');
 const path=require('path');
+// const https=require('https');
 
 const db=require('./util/database')
 const userRouter=require('./routes/user');
@@ -23,12 +24,15 @@ const logStremFile=fs.createWriteStream(path.join(__dirname,'access.log'),{flags
 
 const app=express();
 
+// const serverPrivateKey=fs.readFileSync('server.key');
+// const sslCertificate=fs.readFileSync('server.cert');
+
+
 app.use(bodyParser.json({extended:false}));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cors());
 app.use(helmet());
 app.use(morgan('combined',{stream:logStremFile}));
-
 
 app.use('/user',userRouter);
 
@@ -60,6 +64,11 @@ db.sync()
 .then(result=>{ 
     //PORT value inserted by hosting server
     app.listen(process.env.PORT||3000,()=>console.log("listening to port 3000..."))
+
+    //checking with own signed ssl certificate with this web app// but we do not need to this as browser doesnot accept own signed ssl/tls certidicate
+    //for the development environment we can use it but for the production we need to take SSL/TLS certificate from Certificate Authority which take kare of all the things
+    // https.createServer({key:serverPrivateKey,cert:sslCertificate},app).listen(process.env.PORT||3000,()=>console.log("listening to port 3000..."))
+   
 })
 .catch(err=>console.log(err));
 
