61ad6370d348c4d3834c3e176a0b93d009bd1711
Akshayshah47 <removeit61@gmail.com>
2023-06-29 16:41:00 +0530

    pre work for deployment

--- backEnd/util/database.js
+++ backEnd/util/database.js
@@ -2,6 +2,6 @@ const {Sequelize}=require('sequelize');
 const dotenv=require('dotenv');
 dotenv.config();
 
-const sequelize=new Sequelize(process.env.DATABASE,process.env.SQL_USERNAME,process.env.SQL_PASSWORD,{dialect:'mysql',host:'localhost'});
+const sequelize=new Sequelize(process.env.DATABASE,process.env.SQL_USERNAME,process.env.SQL_PASSWORD,{dialect:'mysql',host:process.env.DB_HOST});
 
 module.exports=sequelize;
